d7e3435dfc865ed1fba08b3e18dc5244d8f49506
alpiazza13 <alexpiazza@Alexs-MacBook-Pro-3.local>
2020-04-15 19:11:51 -0400

    some cleanup

---
+++ helpers.py
@@ -0,0 +1,91 @@
+import binascii
+import os
+import sys
+import glob
+import random
+
+
+
+#makes pop-up
+def notify(title, text):
+    os.system("""
+              osascript -e 'display notification "{}" with title "{}"'
+              """.format(text, title))
+#
+def compress(s):
+    i = 0
+    result = ""
+    while i < len(s):
+        if i % 20 == 0:
+            result += str(s[i])
+        i+=1
+    return result
+
+
+# get name of most recent file in downloads
+def newest_file():
+    list_of_files = glob.glob(os.path.expanduser('~')+"/Downloads/*")
+    latest_file = max(list_of_files, key=os.path.getmtime)
+    return latest_file
+
+def size_downloads():
+    return len(os.listdir(os.path.expanduser('~')+"/Downloads"))
+
+#get a string of hexadecimals of a file
+def get_hex_compressed(filename):
+    with open(filename, 'rb') as f:
+        content = f.read()
+    hex1 = compress(binascii.hexlify(content))
+    str_hex = str(hex1)[2:-1] # get rid of first 2 and last 1 characters
+    return str_hex
+
+
+def make_substrings(size, filename):
+    i = 0
+    substrings = []
+    while i < len(filename) - size - 1:
+        substrings.append(filename[i:i+size])
+        i+=1
+    return substrings
+
+viruses=["virus1.txt", "virus2.txt", "virus3.txt", "try.jpg"]
+latest_download = newest_file()
+download_hex = get_hex_compressed(latest_download)
+
+def compare(hex_file, hex_virus, sub_size):
+    if sub_size > len(hex_file) or sub_size > len(hex_virus):
+        return False
+    # hex_file_subs = make_substrings(sub_size, hex_file)
+    # if any(hex_sub in hex_virus for hex_sub in hex_file_subs):
+    #     return True
+    # return False
+    hex_file_subs = make_substrings(sub_size, hex_file)
+    hex_virus = make_substrings(sub_size, hex_virus)
+    hex_file_subs = set(hex_file_subs)
+    hex_virus = set(hex_virus)
+    return len(hex_file_subs.intersection(hex_virus)) > 0
+
+
+# based on idea that smaller substrings make it way faster, we go from ground up
+# cause most of them will be different with small substrings, and we only check for bigger
+# substrings when the files are very similar
+# We'll do a lot more work for similar files but it will be exponentially faster for different files
+
+#Let's just compress and get hex of viruses before so that we don't have to do it when a file is downloaded
+#it saves time from donwload to result of virus or not
+
+
+viruses_dict = {}
+for virus in viruses :
+    virus_string = get_hex_compressed(virus)
+    viruses_dict[virus] = virus_string
+
+def generate_str(size):
+    s = ""
+    for x in range(size):
+        s += str(random.randint(0,9))
+    return s
+
+big = generate_str(100000)
+
+viruses_dict["big"] = big
--- is_virus.py
+++ is_virus.py
@@ -3,115 +3,56 @@ import os
 import sys
 import glob
 import random
+from helpers import notify, compare, newest_file, get_hex_compressed, size_downloads
+import time
 
-
-def notify(title, text):
-    os.system("""
-              osascript -e 'display notification "{}" with title "{}"'
-              """.format(text, title))
-
-
-def compress(s):
-    #can find better compression but really has to keep the linear pattern, so most compressions don't work
-    # so i feel like what i'm doing if pretty good cause i divide size by a number and keep linear pattern
-    i = 0
-    result = ""
-    while i < len(s):
-        if i % 20 == 0:
-            result += str(s[i])
-        i+=1
-    return result
-
-
-# get the name of the newly downloaded file
-def newest_file():
-    list_of_files = glob.glob(os.path.expanduser('~')+"/Downloads/*")  # goes to the user's root directory
-    latest_file = max(list_of_files, key=os.path.getmtime)      #most recent file in the directory
-    return latest_file
-
-#get a string of hexadecimals of a file
-def get_hex_compressed(filename):
-    with open(filename, 'rb') as f:
-        content = f.read()
-    hex1 = compress(binascii.hexlify(content))
-    str_hex = str(hex1)[2:-1] # get rid of first 2 and last 1 characters
-    return str_hex
-
-
-def make_substrings(size, filename):
-    i = 0
-    substrings = []
-    while i < len(filename) - size - 1:
-        substrings.append(filename[i:i+size])
-        i+=1
-    return substrings
 
 viruses=["virus1.txt", "virus2.txt", "virus3.txt", "try.jpg"]
 latest_download = newest_file()
 download_hex = get_hex_compressed(latest_download)
 
-def compare(hex_file, hex_virus, sub_size):
-    if sub_size > len(hex_file) or sub_size > len(hex_virus):
-        return False
-    # hex_file_subs = make_substrings(sub_size, hex_file)
-    # if any(hex_sub in hex_virus for hex_sub in hex_file_subs):
-    #     return True
-    # return False
-    hex_file_subs = make_substrings(sub_size, hex_file)
-    hex_virus = make_substrings(sub_size, hex_virus)
-    hex_file_subs = set(hex_file_subs)
-    hex_virus = set(hex_virus)
-    return len(hex_file_subs.intersection(hex_virus)) > 0
-
-
-# based on idea that smaller substrings make it way faster, we go from ground up
-# cause most of them will be different with small substrings, and we only check for bigger
-# substrings when the files are very similar
-# We'll do a lot more work for similar files but it will be exponentially faster for different files
-
-#Let's just compress and get hex of viruses before so that we don't have to do it when a file is downloaded
-#it saves time from donwload to result of virus or not
-
-
 viruses_dict = {}
-for virus in viruses :
+for virus in viruses:
     virus_string = get_hex_compressed(virus)
     viruses_dict[virus] = virus_string
 
-def generate_str(size):
-    s = ""
-    for x in range(size):
-        s += str(random.randint(0,9))
-    return s
-
-big = generate_str(100000)
-
-viruses_dict["big"] = big
-
-
-#We're gonna have to put thing in an infinite loop that checks if a file has been downloaded and
-#runs the function if yes
+#check if hex_file is a virus
 def is_virus(hex_file, viruses_str, final_subsize):  #final_subsize has to be carefully evaluated
     for virus in viruses_str:
-        print(virus)
-        sub_size = 16 # we can start with however small, the smallest just means a little more computing
-                      #but faster if file really really different
+        # print(virus)
+        sub_size = 16 #arbitrary small number
         go_on = True
         while go_on:
             if sub_size >= final_subsize:
                 return True
             result = compare(hex_file, viruses_str[virus], sub_size)
-            print("compare", sub_size, result)
-            if result == False :
+            # print("compare", sub_size, result)
+            if result == False:
                 go_on = False
-            sub_size *= 2 # *= whatever we want
+            sub_size *= 2 # kind of arbitrary
     return False
 
-if is_virus(viruses_dict["big"], viruses_dict, 200):
-    notify("VIRUS ALERT", "this is probably a virus, you should delete it!")
 
-# print(is_virus(viruses_dict["big"], viruses_dict, 2000 ))
+def main():
+    newest = newest_file()
+    size_folder = size_downloads()
+    while True:
+        checkfornew = newest_file()
+        new_size_folder = size_downloads()
+        time.sleep(0.5)
+        if new_size_folder >= size_folder:      #check if the new most recent is not due to a deletion
+            if not checkfornew.endswith('.crdownload') and not checkfornew.endswith('.download'):
+                if checkfornew != newest:
+                    result = is_virus(checkfornew, viruses_dict, 200)
+                    if result == True:
+                        notify("VIRUS ALERT", "this is probably a virus, you should delete it!")
+                    else:
+                        notify("VIRUS CHECK", "this is probably a virus, you should delete it!")
+        if not checkfornew.endswith('.crdownload') and not checkfornew.endswith('.download'):
+            newest = checkfornew
+        size_folder = new_size_folder
 
+main()
 
 '''
 Our number final_subsize is the threshold from which if a file an han equal substrings of this size with a
