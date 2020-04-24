4ef8d65e91a0890929036f72522bfb3338de71fc
Samuel Javal <samueljaval@gmail.com>
2020-04-25 00:05:51 +0200

    added forgotten functions

--- os_functions.py
+++ os_functions.py
@@ -4,6 +4,21 @@ import time
 from helpers import fit_to_unix
 
 
+# check that the path is correct up to Downloads
+def secure_path(checkfornew):
+    right_path = os.path.expanduser('~')+"/Downloads/"
+    i = 0
+    while i < len(right_path):
+        if not(checkfornew[i] == right_path[i]):
+            return False
+        i+=1
+    return True
+
+def check_item(checkfornew,item):
+    for char in checkfornew:
+        if char == item:
+            return False
+    return True
 
 def security(checkfornew):
     check1 = check_item(checkfornew,"/-")
