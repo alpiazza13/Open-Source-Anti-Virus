d1cd7cce19bc5a9a74254a1dffae95870edf2d73
Kevin Koech <34930805+kkkoech@users.noreply.github.com>
2020-04-26 21:51:25 -0400

    Update loop_and_onefile.py

--- loop_and_onefile.py
+++ loop_and_onefile.py
@@ -11,16 +11,13 @@ viruses_dict = viruses_fileobj.read()
 viruses_dict = eval(viruses_dict)
 
 '''
-
 To get the viruses for the online github repo, we need to do the following
 When we eventually link, we have to make sure clonning the virus is something safe to do!!!!
 
 from get_viruses import get_viruses_github
 viruses = get_viruses_github()
-
 '''
 # viruses = ["viruses/virus1.txt", "viruses/virus2.txt", "viruses/virus3.txt", "viruses/try.jpg"]
-
 # viruses_dict = formating_viruses(viruses)
 
 def main_loop():
