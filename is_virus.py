492b285620b403322c49d26d8b10b5ff190408d3
Samuel Javal <samueljaval@gmail.com>
2020-04-30 03:09:12 +0200

    cpu cores

--- is_virus.py
+++ is_virus.py
@@ -1,6 +1,7 @@
 from helpers import make_substrings, get_hex_compressed, md5_filehash, split_dict
 import multiprocessing as mp
 
+
 def compare(hex_file, hex_virus, sub_size):
     if sub_size > len(hex_file) or sub_size > len(hex_virus):
         return False
@@ -53,7 +54,7 @@ def multiprocess_check(hex_file, viruses, final_subsize):
     manager = mp.Manager()
     return_dict = manager.dict()
     list_processes = []
-    viruses_divided = split_dict(viruses,5)  #5 should maybe be a variable
+    viruses_divided = split_dict(viruses,mp.cpu_count()+1)
     # starting processes
     process_nb = 0
     for portion in viruses_divided:
@@ -69,7 +70,6 @@ def multiprocess_check(hex_file, viruses, final_subsize):
         return True
     else :
         return False
-
 
 '''
 Our number final_subsize is the threshold from which if a file an han equal substrings of this size with a
